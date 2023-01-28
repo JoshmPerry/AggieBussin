9ef0d6cae64cf0f30f9bd8c719da63cee6a69798
Joshua Perry <josh.m.perry@gmail.com>
2023-01-28 15:50:30 -0600

    modified:   runner.py

--- runner.py
+++ runner.py
@@ -1,14 +1,20 @@
 import pymongo
-# Replace the uri string with your MongoDB deployment's connection string.
+import scraper.py
+
 conn_str = "mongodb+srv://JoshmPerry:duPLYKDpBM11HfbB@cluster0.cana0cv.mongodb.net/test?retryWrites=true&w=majority"
 
 client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
 database = client["mydatabase"]
-collection = database["oplog.rs"]
-#customers = database["customers"]
-#customers.drop()
-#collection.insert_one({"_id":1,"job":"Python"})
+collection = database["buses"]
 
-print(collection)
+
+
+
+collection.drop()
+for curbus in :
+    dictionaryToAdd={"_id":curbus.getName().split()[0],"BusName":curbus.getName(),"Stops":curbus.getStops(),"Today":bool(curbus.getStops())}
+    collection.insert_one(dictionaryToAdd)
+
+
 print("finished")
 
