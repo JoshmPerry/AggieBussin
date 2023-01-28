72f9053d94e83e34c14a03a6967b3136c50dbdde
Joshua Perry <josh.m.perry@gmail.com>
2023-01-28 12:51:42 -0600

    modified:   runner.py

--- runner.py
+++ runner.py
@@ -1,9 +1,14 @@
 import pymongo
 # Replace the uri string with your MongoDB deployment's connection string.
-conn_str = "mongodb+srv://<username>:<password>@<cluster-address>/test?retryWrites=true&w=majority"
-# set a 5-second connection timeout
+conn_str = "mongodb+srv://JoshmPerry:duPLYKDpBM11HfbB@cluster0.cana0cv.mongodb.net/test?retryWrites=true&w=majority"
+
 client = pymongo.MongoClient(conn_str, serverSelectionTimeoutMS=5000)
-try:
-    print(client.server_info())
-except Exception:
-    print("Unable to connect to the server.")
+database = client["mydatabase"]
+collection = database["oplog.rs"]
+#customers = database["customers"]
+#customers.drop()
+#collection.insert_one({"_id":1,"job":"Python"})
+
+print(collection.)
+print("finished")
+
