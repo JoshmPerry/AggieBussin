37181597c74e7b3c9bb10a6437038c118c34e1f9
Joshua Perry <josh.m.perry@gmail.com>
2023-02-05 22:11:23 -0600

    modified:   route_maker.js

--- route_maker.js
+++ route_maker.js
@@ -124,45 +124,67 @@ async function soonestRoutesAnywhere(fromt,tot,depT,arivT){
   return fastperbus;
 }
 
+async function searchRoute(deep,givenlocation,wantedRoutes){
+
+  
+}
+
 async function findRoute(fromt,tot,Ltime){
   var midroutes=[];
-  midroutes[0]=[0,await soonestRoutesAnywhere(fromt,"",Ltime,0)];
+  midroutes[0]=[[-1,[await soonestRoutesAnywhere(fromt,"",Ltime,0)]]];
   var solutions=await getRoute("",tot,0,0);
-  //console.log(midroutes[0][1]);
-  var tmp = midroutes[0][1].filter(function(route){for(var t=0;t<solutions.length;t++){if(solutions[t]._id===route._id){return true;}}return false;});
+  //console.log(midroutes[0][1][0],Array.isArray(midroutes[0][1][0]));
+  var tmp = midroutes[0][0][1][0].filter(function(route){for(var t=0;t<solutions.length;t++){if(solutions[t]._id===route._id){return true;}}return false;});
   //console.log(tmp);
   if(tmp.length>0){
     //console.log("Why?");
     return tmp;
   }
+  for(var depth=1;depth<4;depth++){
+    console.log("Made it1");
 
-  for(var depth=1;depth<5;depth++){
-    var tx=[];
-    for(var route=1;route<midroutes[depth-1][1].length;route++){
-      //console.log("Route: ",midroutes[depth-1][route][1]);
-      tx.push([route,await soonestRoutesAnywhere(midroutes[depth-1][route][1]["To"],"",midroutes[depth-1][route][1]["Arrival"],0)]);
+    var tx=[];//This should not be here
+    //console.log("Routes for",depth,midroutes);
+    //console.log(midroutes[depth-1]);
+    for(var route=1;route<midroutes[depth-1][0].length;route++){
+      //console.log(route,midroutes[depth-1][0][route][0]);
+      console.log("Made it2");
 
-      //console.log(tx[route-1]);
-      
+      for(var route2=0;route2<midroutes[depth-1][0][route][0].length;route2++){
+        console.log("Made it3");
 
-      tmp = tx[route-1][1].filter(function(route){for(var t=0;t<solutions.length;t++){if(solutions[t]._id===route._id){return true;}}return false;});
+      //console.log("Route: ",midroutes,depth-1);
+      //console.log(midroutes[depth-1][0][route][0][route2],depth,route,route2);
+      //console.log("now");
+      tx.push([route2,[await soonestRoutesAnywhere(midroutes[depth-1][0][route][0][route2]["To"],"",midroutes[depth-1][0][route][0][route2]["Arrival"],0)]]);
+      //console.log(midroutes);
+      //console.log(route,route2);
+      //console.log(await soonestRoutesAnywhere(midroutes[depth-1][1][route2]["To"],"",midroutes[depth-1][1][route2]["Arrival"],0));
+
+      tmp = tx[route2][1][route-1].filter(function(route){for(var t=0;t<solutions.length;t++){if(solutions[t]._id===route._id){return true;}}return false;});
       //console.log(tmp);
       if(tmp.length>0){
+
         console.log("Yay.",route);
         return tmp;
       }
 
     }
-    midroutes[depth]=tx;
-    console.log("Midroutes");
-    console.log(tx);
 
+
+  }
+    //console.log("Route",depth,tx);
+    midroutes[depth]=tx;
+    //console.log("Midroutes",midroutes);
+    //console.log(tx);
+  
     
 
   }
-
+console.log(midroutes);
+return"Error,Didn't find";
 }
 
-findRoute("Trigon","Laurel Ridge",780).then(value=>{console.log(value);});
+findRoute("Trigon","Holleman Oaks - North",0).then(value=>{console.log(value);});
 //getRoute("HSC","",0,0).then(value=>{console.log(value);});
 
